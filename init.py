95f51068261ecf3261e68a8c474a50ac564f69d9
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-08-06 09:01:44 +0200

    add init

---
+++ init.py
@@ -0,0 +1,7 @@
+from .person import Person
+from .student import Student
+from .teacher import Teacher
+from .course import Course
+from .director import Director
+
+__all__ = ['Person', 'Student', 'Teacher', 'Course', 'Director']
